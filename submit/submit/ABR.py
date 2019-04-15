ae9e7c613bf9349204a9ea940af56e3ee11f8849
yilengyanhua <1208271687@qq.com>
2019-04-15 21:48:17 +0800

    Add S_skip_time as an input

--- submit/submit/ABR.py
+++ submit/submit/ABR.py
@@ -15,11 +15,12 @@ class Algorithm:
              return IntialVars
 
      #Define your algorithm
-     def run(self, time, S_time_interval, S_send_data_size, S_chunk_len, S_rebuf, S_buffer_size, S_play_time_len,S_end_delay, S_decision_flag, S_buffer_flag,S_cdn_flag, end_of_video, cdn_newest_id,download_id,cdn_has_frame, IntialVars):
+     def run(self, time, S_time_interval, S_send_data_size, S_chunk_len, S_rebuf, S_buffer_size, S_play_time_len,S_end_delay, S_decision_flag, S_buffer_flag,S_cdn_flag,S_skip_time, end_of_video, cdn_newest_id,download_id,cdn_has_frame, IntialVars):
     
          bit_rate = 1
          target_buffer = 1
-         return bit_rate, target_buffer
+         latency_limit = 7
+         return bit_rate, target_buffer, latency_limit
 
 
          # If you choose other
